6e4b7db07fffcb7199fa4c798fb0b84ecd280797
Nagasudhir Pulla <nagasudhirpulla@gmail.com>
2025-01-29 07:32:04 +0530

    logout implemented

---
+++ Web/Pages/Logout.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            // TODO remove hard coded URL, use nameof instead
+            return RedirectToPage("/Index");
+        }
+    }
+}
